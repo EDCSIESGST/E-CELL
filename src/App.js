4c82572a6bdfc8964b9bdda91a77251bba438d5b
Yash Varma <yashvarma2702@gmail.com>
2024-10-15 23:52:54 +0530

    RevivedTheWebPage

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import Startups from './Pages/Startups/Startups';
 import Reports from './Pages/Reports/Reports';
 import Community from './Pages/Community/Community';
 import Team from './Pages/Team/team';
-import Bizence from './Pages/Bizence/Bizence';
+// import Bizence from './Pages/Bizence/Bizence';
 import Eventsbrief from './Pages/Eventsbrief/Eventsbrief';
 import Home from './Pages/Homepage/Home';
-<li className="nav-item">
-<Link className="nav-link" to="#banner">Home</Link>
-</li>
+import { Link } from 'react-router-dom';
+
+import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import { Route } from 'react-router-dom';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // Import AOS CSS
 import './App.css'
+
+
+
+<li className="nav-item">
+<Link className="nav-link" to="#banner">Home</Link>
+</li>
 function App() {
   useEffect(() => {
     AOS.init({
@@ -26,7 +32,7 @@ function App() {
 
   return (
     <div>
-     
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Startups" element={<Startups />} />
@@ -34,7 +40,7 @@ function App() {
         <Route path="/EventsBrief" element={<Eventsbrief />} />
         <Route path="/Reports" element={<Reports />} />
         <Route path="/Team" element={<Team />} />
-        <Route path="/Bizence" element={<Bizence />} />
+        {/* <Route path="/Bizence" element={<Bizence />} /> */}
       </Routes>
       <Footer />
     </div>
